07563a8e399cb35ae620ec806ed72e1e34964596
Richard Weber <richard.weber@ksfmedia.fi>
2015-01-23 13:33:08 +0200

    jenkins CI test

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ var PORT = 8080;
 // App
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Hello world, I am on docker!\n');
+  res.send('Hello world, I am on docker!\n And I am build with Jenkins!');
 });
 
 app.listen(PORT);
