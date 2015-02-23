d8063498f777e669465b0700146d29dff29943bf
Richard Weber <richard.weber@ksfmedia.fi>
2015-02-23 14:41:23 +0200

    generic

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ var PORT = 8080;
 // App
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Hello world, I am on docker!\n And I am build with Jenkins!');
+  res.send('Hello world, I am on docker!');
 });
 
 app.listen(PORT);
